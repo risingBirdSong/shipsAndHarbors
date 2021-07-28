9cd4024394906c44e86738d1bad43684765d0eef
Peter Murphy <heregoingthere@outlook.com>
2021-07-27 21:07:06 -0700

    ships can now leave harbor and showing the names of ships and harbors when they leave or enter. But notice the nesting case ofs in postShipLeavesHarborR, that isnt great

--- src/Handler/Boatsharbors.hs
+++ src/Handler/Boatsharbors.hs
@@ -1,61 +1,78 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Handler.Boatsharbors where
 
 
 import Import
-import Database.Esqueleto hiding (on, from)
+import Database.Esqueleto hiding (on, from, (==.), Value, delete)
 import qualified Database.Esqueleto.Experimental as E
 
-getAllShipsR :: Handler Import.Value
+getAllShipsR :: Handler Value
 getAllShipsR = do
    ships :: [Entity Ship] <- runDB $ selectList [] []
    returnJson $ object $ [(pack "ships") .= ships ]
 
-postNewShipR :: Handler Import.Value
+postNewShipR :: Handler Value
 postNewShipR = do 
     ship@Ship {..} <- requireCheckJsonBody :: Handler Ship 
     _ <- runDB $ insert ship
     sendResponseStatus status201 ("CREATED SHIP" :: String)
 
-getAllHarborsR :: Handler Import.Value 
+getAllHarborsR :: Handler Value 
 getAllHarborsR = do 
    harbors :: [Entity Harbor] <- runDB $ selectList [] []
    returnJson $ object $ [(pack "harbors") .= harbors]
 
-postNewHarborR :: Handler Import.Value
+postNewHarborR :: Handler Value
 postNewHarborR = do
     harbor@(Harbor {..}) <- requireCheckJsonBody
     _ <- runDB $ insert harbor
     sendResponseStatus status201 ("CREATED HARBOR" :: String)
 
 
-postShipVisitHarborR :: Text -> Text -> Handler Import.Value 
+postShipVisitHarborR :: Text -> Text -> Handler Value 
 postShipVisitHarborR shipname harborname = do 
-   mShip <- runDB $ selectFirst [ShipName Import.==. shipname] [] 
-   mHarbor <- runDB $ selectFirst [HarborName Import.==. harborname] []
+   mShip <- runDB $ selectFirst [ShipName ==. shipname] [] 
+   mHarbor <- runDB $ selectFirst [HarborName ==. harborname] []
    case (mShip, mHarbor) of
-        (Just eship@(Entity sid ship), Just eharbor@(Entity hid harbor)) -> do
-            _ <- runDB $ insert $ HarborShip (hid) sid
-            returnJson $ object [(pack "ship" .= ship), (pack "harbor" .= harbor)]
+        (Just (Entity sid ship), Just (Entity hid harbor)) -> do
+            _ <- runDB $ insert $ Docking (hid) sid
+            sendResponseStatus status201 ("The ship " <> (shipName ship) <> "has arrived in the Harbor " <> (harborName harbor) :: Text)
         _ -> sendResponseStatus status201 ("NOT FOUND" :: String)
 
+postShipLeavesHarborR :: Text -> Text -> Handler Value
+postShipLeavesHarborR shipname harborname = do
+   mHarbor <- runDB $ selectFirst [HarborName ==. harborname] []
+   mShip <- runDB $ selectFirst [ShipName ==. shipname] []
+   case (mHarbor, mShip) of 
+        (Just (Entity hid harbor), Just (Entity sid ship)) -> do
+            mDocking <- runDB $ selectFirst ([DockingHarborId ==. hid] ++ [DockingShipId ==. sid]) []
+            case mDocking of
+                (Just (Entity dId docking)) -> do
+                    _ <- runDB $ delete dId 
+                    sendResponseStatus status201 ((shipName ship ) <> " has left the harbor " <> (harborName harbor) :: Text) 
+                Nothing -> sendResponseStatus status201 ("That ship was not at the harbor" :: String)   
+        _ -> sendResponseStatus status201 ("Either the ship or the harbor was not found" :: String) 
 
-getShipsAtHarborR :: Text -> Handler Import.Value 
+
+getShipsAtHarborR :: Text -> Handler Value 
 getShipsAtHarborR harborname = do 
     ships <- runDB $ select $ do 
-        (ships E.:& harborship E.:& harbors) <- 
+        (ships E.:& docking E.:& harbors) <- 
             E.from $ E.Table @Ship
-            `E.InnerJoin` E.Table @HarborShip
-            `E.on` (\(ship E.:& harborship) -> 
-                ship ^. ShipId E.==. harborship ^. HarborShipShipId)
+            `E.InnerJoin` E.Table @Docking
+            `E.on` (\(ship E.:& docking) -> 
+                ship ^. ShipId E.==. docking ^. DockingShipId)
             `E.InnerJoin` E.Table @Harbor
-            `E.on` (\ (_ E.:& harborship E.:& harbor) -> 
-                harborship ^. HarborShipHarborId E.==. harbor ^. HarborId
+            `E.on` (\ (_ E.:& docking E.:& harbor) -> 
+                docking ^. DockingHarborId E.==. harbor ^. HarborId
                 )
         where_ (harbors ^. HarborName E.==. val harborname)
         pure (ships)
     returnJson $ object $ [(pack "ships") .= ships]
+
