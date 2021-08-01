5b110ee1fee7d62b3ee08ede63c7782ec606e77a
Peter Murphy <heregoingthere@outlook.com>
2021-08-01 12:16:29 -0700

    replaced tedious pattern matching with getBy404

--- src/Handler/Boatsharbors.hs
+++ src/Handler/Boatsharbors.hs
@@ -44,27 +44,41 @@ postNewShipR = do
     _ <- runDB $ insert ship
     sendResponseStatus status201 ("CREATED SHIP" :: String)
 
-data GetAllHarbors = GetAllHarbors {harbors :: [Entity Harbor]}
+data Harbors = Harbors {harbors :: [Entity Harbor]}
     deriving Generic
 
-instance FromJSON GetAllHarbors
-instance ToJSON GetAllHarbors
+instance FromJSON Harbors
+instance ToJSON Harbors
+
+data AHarbor = AHarbor {harbor :: Entity Harbor }
+    deriving Generic
+
+instance ToJSON AHarbor
+instance FromJSON AHarbor
 
 getAShipR :: Text -> Handler Value
-getAShipR name = do 
+getAShipR name = do
+    -- myTODO getBy404 is sending back a ton of HTML, i'd rather have just a nice and simple json response. so make my own version of getBy404
     gotten <- runDB $ getBy404 $ UniqueShip name 
     returnJson $ AShip {ship = gotten}
 
+getAHarbor :: Text -> Handler Value
+getAHarbor name = do
+    harbor <- runDB $ getBy404 $ UniqueHarbor name
+    returnJson $ AHarbor {harbor = harbor}
+
 getAllHarborsR :: Handler Value 
 getAllHarborsR = do 
-   harbors<- runDB $ selectList [] []
-   returnJson $ GetAllHarbors {harbors = harbors}
+   harbors <- runDB $ selectList [] []
+   returnJson $ Harbors {harbors = harbors}
 
 postNewHarborR :: Handler Value
 postNewHarborR = do
-    harbor@(Harbor {..}) <- requireCheckJsonBody
+    -- can do these ways, top is favorite
+    harbor :: Harbor <- requireCheckJsonBody
+    -- harbor <- requireCheckJsonBody :: Handler Harbor
     _ <- runDB $ insert harbor
-    sendResponseStatus status201 ("CREATED HARBOR" :: String)
+    sendResponseStatus status201 ("Created Thing" :: Text)
 
 -- delete all is for development
 deleteAllHarborsR :: Handler Value
@@ -74,6 +88,7 @@ deleteAllHarborsR = do
 
 deleteShipR :: Text -> Handler Value
 deleteShipR name = do
+    -- myTODO replace with getBy404
     meShip <- runDB $ selectFirst [ShipName ==. name] []
     case meShip of
         Nothing -> sendResponseStatus status201 ("Ship not found, not deleted" :: String) 
@@ -86,28 +101,21 @@ deleteAllShipsR = do
 
 postShipVisitHarborR :: Text -> Text -> Handler Value 
 postShipVisitHarborR shipname harborname = do 
-   mShip <- runDB $ selectFirst [ShipName ==. shipname] [] 
-   mHarbor <- runDB $ selectFirst [HarborName ==. harborname] []
-   case (mShip, mHarbor) of
-        (Just (Entity sid ship), Just (Entity hid harbor)) -> do
-            _ <- runDB $ insert $ Docking (hid) sid
-            sendResponseStatus status201 ("The ship " <> (shipName ship) <> "has arrived in the Harbor " <> (harborName harbor) :: Text)
-        _ -> sendResponseStatus status201 ("NOT FOUND" :: String)
+    -- myTODO try to extract the following two lines into a helper function
+    ship <- runDB $ getBy404 $ UniqueShip shipname
+    harbor <- runDB $ getBy404 $ UniqueHarbor harborname
+    _ <- runDB $ insert $ Docking (entityKey harbor) (entityKey ship)
+    sendResponseStatus status201 ("The ship " <> "has arrived in the Harbor " :: Text)
 
-postShipLeavesHarborR :: Text -> Text -> Handler Value
+postShipLeavesHarborR :: Text -> Text -> Handler ()
 postShipLeavesHarborR shipname harborname = do
-   mHarbor <- runDB $ selectFirst [HarborName ==. harborname] []
-   mShip <- runDB $ selectFirst [ShipName ==. shipname] []
-   case (mHarbor, mShip) of 
-        (Just (Entity hid harbor), Just (Entity sid ship)) -> do
-            mDocking <- runDB $ selectFirst ([DockingHarborId ==. hid] ++ [DockingShipId ==. sid]) []
-            case mDocking of
-                (Just (Entity dId docking)) -> do
-                    _ <- runDB $ delete dId 
-                    sendResponseStatus status201 ((shipName ship ) <> " has left the harbor " <> (harborName harbor) :: Text) 
-                Nothing -> sendResponseStatus status201 ("That ship was not at the harbor" :: String)   
-        _ -> sendResponseStatus status201 ("Either the ship or the harbor was not found" :: String) 
-    
+    (Entity sid ship) <- runDB $ getBy404 $ UniqueShip shipname
+    (Entity hid harbor) <- runDB $ getBy404 $ UniqueHarbor harborname
+    docking <- runDB $ getBy404 $ UniqueDocking hid sid
+    deleted <- runDB $ delete (entityKey docking)
+    -- print "deleted" prints unit ()
+    sendResponseStatus status201 (shipName ship <> " has left " <> harborName harbor)
+-- myAdvancedTODO make a travel function that will handle a ship leaving harbor, travelling (based on travel speed and the distance in coords) and placing the ship in harbor
 
 getShipsAtHarborR :: Text -> Handler Value 
 getShipsAtHarborR harborname = do 
